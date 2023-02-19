8e0057d84c132f9fd7682cfaaadc4aa8da5c01ac
Harshini BS <86827401+harshubs@users.noreply.github.com>
2023-02-19 11:42:07 +0530

    Create new.cpp

---
+++ main/new.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "PES2UG20CS135"\n";
+  cout << "HARSHINI BS\n";
+  return 0;
+}
